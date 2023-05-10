dbd809178047ff10e626cddd5e9f45602142809c
bangpham <uzumakinarutoshin@gmail.com>
2023-05-10 19:57:20 +0700

    edit link render to vercel

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,12 +6,12 @@ const routes = {
     latest: '/latest',
     myList: '/my-list',
     browserByLanguge: '/original-audio',
-    login: 'https://shin-backend.onrender.com/YourAccount/login',
-    register: 'https://shin-backend.onrender.com/YourAccount/register',
-    infoAccount: 'https://shin-backend.onrender.com/YourAccount/',
-    // login: 'http://localhost:3001/YourAccount/login',
-    // register: 'http://localhost:3001/YourAccount/register',
-    // infoAccount: 'http://localhost:3001/YourAccount/',
+    login: 'https://szhinbe.vercel.app/renderLogin',
+    register: 'https://szhinbe.vercel.app/renderRegiser',
+    infoAccount: 'https://szhinbe.vercel.app/YourAccount',
+    // login: 'http://localhost:8080/renderLogin',
+    // register: 'http://localhost:8080/renderRegiser',
+    // infoAccount: 'http://localhost:8080/YourAccount/',
 };
 
 export default routes;
